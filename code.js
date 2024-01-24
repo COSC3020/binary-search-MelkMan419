9c6ba7e5ceb0a9bfbc18af620f72666286eec8cc
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-01-24 16:01:03 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,15 +6,18 @@ function binarySearch(list, element) {
         const mid = Math.floor((left + right) / 2);
 
         if (list[mid] === element) {
+            // Adjust the index if there are multiple occurrences
+            while (mid > 0 && list[mid - 1] === element) {
+                mid--;
+            }
             return mid; // Element found, return its index
-        }
-        else if (list[mid] < element) {
+        } else if (list[mid] < element) {
             left = mid + 1; // Search in the right half
-        }
-        else {
+        } else {
             right = mid - 1; // Search in the left half
         }
     }
+
     // Element not found in the list, return the index where it should be inserted
     return -1;
 }
