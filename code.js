46732c98dc8b71b37912c67af7c802841745411e
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-01-25 11:30:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,17 +3,15 @@ function binarySearch(arr, target) {
   let end = arr.length - 1
   while (start <= end) {
     let middle = Math.floor((start + end) / 2)
-    if (arr[middle] < target) {
-      // Search the right half
+    if (arr[middle] < target) { // Search the right half
       start = middle + 1
-    } else if (arr[middle] > target) {
-      // Search the left half
+    }
+    else if (arr[middle] > target) { // Search the left half
       end = middle - 1
-    } else if (arr[middle] === target) {
-      // Found target
+    } 
+    else if (arr[middle] === target) { // Found target
       return middle
     }
-  }
-  // Target not found
+  } // Target not found
   return -1
 }
